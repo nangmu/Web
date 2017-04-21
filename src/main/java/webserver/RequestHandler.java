d9c1c2c90cb2c3073ed80ed4b04b3bce374b8695
Si-uk <tldnr66@naver.com>
2017-04-21 20:08:42 +0900

    http://localhost:8080/index.html로 접속시 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +23,28 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    private String getRequestResource(String requestLine){
+    	if(requestLine==null) return null;
+    	String[] tokens = requestLine.split(" ");
+    	return tokens[1];
+    	
+    }
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
+        BufferedReader br = null;
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            br = new BufferedReader(new InputStreamReader(in));
+            String requestResource = getRequestResource(br.readLine());
+            if(requestResource ==null){
+            	log.debug("Client HttpRequest Error");
+            	return;
+            }
+            Path path = new File("./webapp"+requestResource).toPath();
+            byte[] body = Files.readAllBytes(path);
+            
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
